73d4f3f944cf8a4f1956f58c0243c79a1bbf98b1
Nabeel A <Nalkhati@my.loyno.edu>
2022-08-09 18:41:16 -0500

    ArcShooting

---
+++ Assets/BasicMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour {
+	
+	public enum motionDirection{Horizontal, Vertical, Spin};
+	public motionDirection motionState;
+	public float spinSpeed = 180.0f;
+	public float motionMagintude =0.1f;
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (motionState == motionDirection.Spin)
+			gameObject.transform.Rotate (Vector3.up * spinSpeed * Time.deltaTime);
+		if(motionState == motionDirection.Vertical)
+			gameObject.transform.Translate (Vector3.up * Mathf.Cos (Time.timeSinceLevelLoad) * motionMagintude);
+		if(motionState == motionDirection.Horizontal)
+			gameObject.transform.Translate (Vector3.right * Mathf.Cos (Time.timeSinceLevelLoad) * motionMagintude);
+	}
+}
---
+++ Assets/playerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	public float movementSpeed = 3.0f;
+	public float gravity = 9.8f;
+
+	private CharacterController myController;
+	// Use this for initialization
+	void Start () {
+		myController = gameObject.GetComponent<CharacterController> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 movementZ = Input.GetAxis ("Vertical") * Vector3.forward * movementSpeed * Time.deltaTime;
+		Vector3 movementX = Input.GetAxis ("Horizontal") * Vector3.right * movementSpeed * Time.deltaTime;
+
+		Vector3 movement = transform.TransformDirection(movementX + movementZ);
+		movement.y -= gravity *Time.deltaTime; 
+
+		myController.Move(movement);
+	}
+}
